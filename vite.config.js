a4b7607204e702450f68723e7591b5ec267a17eb
Bekzod <mirzaahmedov.dev@gmail.com>
2022-07-14 22:28:28 +0500

    initial

---
+++ vite.config.js
@@ -0,0 +1,18 @@
+import path from "path";
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: {
+      "@views": path.resolve(__dirname, "src/views"),
+      "@components": path.resolve(__dirname, "src/components"),
+      "@assets": path.resolve(__dirname, "src/assets"),
+      "@utils": path.resolve(__dirname, "src/utils"),
+      "@hooks": path.resolve(__dirname, "src/hooks"),
+      "@context": path.resolve(__dirname, "src/context"),
+    },
+  },
+});
